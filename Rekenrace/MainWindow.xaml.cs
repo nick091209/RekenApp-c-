7dde96aa0322765140294dfa8a8959ac78d8cd3c
nick <nickcolijn2006@gmail.com>
2025-04-01 23:00:50 +0200

    add files

---
+++ Rekenrace/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace RekenApplicatie
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            string spelerNaam = NaamTextBox.Text.Trim();
+            if (string.IsNullOrEmpty(spelerNaam))
+            {
+                MessageBox.Show("Vul een naam in!");
+                return;
+            }
+
+            // Open het rekenscherm en sluit het huidige venster
+            Rekenscherm rekenscherm = new Rekenscherm(spelerNaam);
+            rekenscherm.Show();
+            this.Close();
+        }
+    }
+}
